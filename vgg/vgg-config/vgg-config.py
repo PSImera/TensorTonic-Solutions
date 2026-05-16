38f04ba90df68cda739a79f50f37ad87763da3a0
Vladislav <131370434+PSImera@users.noreply.github.com>
2026-05-16 23:16:24 +0400

    Add solution for vgg-config

---
+++ vgg/vgg-config/vgg-config.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def make_vgg_config(variant: str) -> list:
+    """
+    Return the layer configuration for a VGG variant.
+    """
+    dict = {
+        "vgg11": [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], 
+        "vgg13": [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'], 
+        "vgg16": [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'], 
+        "vgg19": [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M']
+    }
+
+    return dict[variant.lower()]
